21c132aa6c710eeae8009f851bfb05b208d6a8ba
Basirat Hamayoon <basirathumayun@gmail.com>
2025-04-06 12:01:28 +0500

    Create User Model

---
+++ Backend/models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true
+    },
+    passowrd: {
+        type: String ,
+        required: true,
+    },
+    bio : {
+        type : String
+    },
+    role : {
+        type : String, 
+        enum : [ 'user', 'admin' ],
+        default : 'user'
+    },
+    savedPosts : [
+        {
+            type : mongoose.Schema.Types.ObjectId, ref: 'Post'
+        }
+    ],
+    createdAt : {
+        type : Date,
+        default : Date.now
+    }
+});
+
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('passowrd')) return next();
+    this.passowrd = await bcrypt.hash(this.passowrd, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.passowrd);
+};
+
+module.exports = mongoose.model('User', userSchema);
