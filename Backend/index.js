063fce1c6465dd2ebb0f17ba0ba797d1b9388e30
Basirat Hamayoon <basirathumayun@gmail.com>
2025-04-06 11:35:08 +0500

    Create a mongoDB connection and also run the server

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 const cors = require('cors');
+// const errorHandler = require('./middlewares/errorHandler.js');
+const connectDB = require('./config/db');
+
+//Routes
+// const authRoutes = require('./routes/auth.js');
+// const postRoutes = require('./routes/post.js');
+// const userRoutes = require('./routes/user.js');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/posts', postRoutes);  
+// app.use('/api/users', userRoutes);
+
+// app.use(errorHandler);
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`http://localhost:${port} is now running!`));
