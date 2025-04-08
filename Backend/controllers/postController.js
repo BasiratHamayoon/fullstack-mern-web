c89b9f8fe348241b9cefa4f25cd6d32230511d19
Basirat Hamayoon <basirathumayun@gmail.com>
2025-04-08 22:34:35 +0500

    Fix the bug in post controller

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -2,7 +2,7 @@ const Post = require('../models/Post.js');
 const catchAsync = require('../utils/catchAsync.js');
 const appError = require('../utils/AppError.js');
 
-const createPost = catchAsync(async (req, resizeBy, next) => {
+const createPost = catchAsync(async (req, res, next) => {
     const post = await Post.create({ ...req.body, author: req.user._id });
     res.status(201).json(post);
 });
