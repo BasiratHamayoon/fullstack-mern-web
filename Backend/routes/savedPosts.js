cc0772e5c63f63121005b912b3ceda6bf7969113
Basirat Hamayoon <basirathumayun@gmail.com>
2025-04-12 18:00:46 +0500

    Create getAllPosts controller using aggregation lookups

--- Backend/routes/savedPosts.js
+++ Backend/routes/savedPosts.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { protect } = require('../middlewares/authMiddleware.js');
-const { savePost } = require('../controllers/savedPostsController.js');
+const { savePost, getSavedPosts } = require('../controllers/savedPostsController.js');
 
 const router = express.Router();
 
 router.post('/post/:postId/save', protect, savePost);
+router.get('/users/saved-posts', protect, getSavedPosts)
+
 
 module.exports = router;
